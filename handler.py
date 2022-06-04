51819628487cbc523521927d713421d546e709b9
GabrielMeelo97 <gabrielmelo_@outlook.com>
2022-06-04 15:02:16 -0300

    ajustando sintaxe

--- handler.py
+++ handler.py
@@ -1,14 +1,11 @@
+import logging
+from datetime import datetime
+from tempfile import NamedTemporaryFile
+
+import boto3
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
-import pandas as pd
-from tempfile import NamedTemporaryFile
-from datetime import datetime
-import json
-import boto3
-import logging
-from os import environ as env
-
-
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -20,14 +17,14 @@ def request_inicial():
     soup = bs(response.text)
     return soup
 
+
 def buscar_categorias(soup):
     categorias_full = []
     for categoria in soup.find_all('li'):
         try:
-            print(categoria.find('a').get('href'))
             categorias_full.append(categoria.find('a').get('href'))
         except:
-            print('erro')
+            logger.info(f"Encontrei um problena na coleta da categoria {categoria}")
     return categorias_full
 
 
@@ -39,72 +36,77 @@ def acertar_categorias(lista):
             categorias.append(i)
     return categorias
 
+
 def acerta_url(url):
-    paginas = range(1,1000000000000000000000000000000)
+    paginas = range(1, 1000000000000000000000000000000)
     numero_paginas = 0
     for i in paginas:
-        url_2 = url.replace('index','page-{}.html')
+        url_2 = url.replace('index', 'page-{}.html')
         livro_response = requests.get(url_2.format(5))
-        if livro_response.ok == True:
+        if livro_response.ok:
             numero_paginas = numero_paginas + 1
         else:
             break
     if numero_paginas >= 1:
-        return url_2,numero_paginas
-    else: 
-        return url,numero_paginas
+        return url_2, numero_paginas
+    else:
+        return url, numero_paginas
 
 
-def request_api(url,categoria):
+def request_api(url, categoria):
     livro_response = requests.get(url)
     livros = bs(livro_response.text)
     dados = []
-    for livro in livros.find_all('li',{'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'}):
+    for livro in livros.find_all('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'}):
         nome_produto = livro.find('h3').find('a').get('title')
         print(nome_produto)
-        preco = livro.find('p',{'class':'price_color'}).text[2:]
-        estoque = livro.find('p',{'class':'instock availability'}).text.strip()
-        pontuacao = str(livro.find('article',{'class':'product_pod'}).find('p'))[:30].split('"')[1].replace('star-rating ','')
-        dados.append([categoria,nome_produto,preco,estoque,pontuacao])
+        preco = livro.find('p', {'class': 'price_color'}).text[2:]
+        estoque = livro.find('p', {'class': 'instock availability'}).text.strip()
+        pontuacao = str(livro.find('article', {'class': 'product_pod'}).find('p'))[:30].split('"')[1].replace('star-rating ', '')
+        dados.append([categoria, nome_produto, preco, estoque, pontuacao])
     return dados
 
 
 def coleta_dados(categorias):
     dados_livros = []
+    logger.info("Coletando os livros dentro das categorias")
     for categoria in categorias:
-        url = 'https://books.toscrape.com/catalogue/'+ categoria
-        url_validada,paginas = acerta_url(url)
+        url = 'https://books.toscrape.com/catalogue/' + categoria
+        url_validada, paginas = acerta_url(url)
         cagoteria_nome = categoria[15:].split('_')[0]
         if paginas >= 1:
             for i in paginas:
-                lista_dados = request_api(url_validada.format(i),cagoteria_nome)
+                lista_dados = request_api(url_validada.format(i), cagoteria_nome)
                 dados_livros.append(lista_dados)
         else:
-            lista_dados = request_api(url,cagoteria_nome)     
-            dados_livros.append(lista_dados)                                      
+            lista_dados = request_api(url, cagoteria_nome)
+            dados_livros.append(lista_dados)
+    logger.info("Livros coletados")
     return dados_livros
 
-def cria_dataframe(dados):
-    logger.info(f"Concatenando coleta das categorias e paginas")
-    for i,k in enumerate(dados):
-        if i == 0:
-            df_livros = pd.DataFrame(k,columns=['categoria','nome','preco','status_estoque','estrelas'])
-        else: 
-            df_livros = pd.concat([df_livros,pd.DataFrame(k,columns=['categoria','nome','preco','status_estoque','estrelas'])], axis = 0)
-    return df_livros
 
+def cria_dataframe(dados):
+    logger.info("Concatenando coleta das categorias e paginas")
+    for i, k in enumerate(dados):
+        if i == 0:
+            df_livros = pd.DataFrame(k, columns=['categoria', 'nome', 'preco', 'status_estoque', 'estrelas'])
+        else:
+            df_livros = pd.concat([df_livros, pd.DataFrame(k, columns=['categoria', 'nome', 'preco', 'status_estoque', 'estrelas'])], axis=0)
+    logger.info(f"{df_livros.shape[0]} livros coletados")
+    return df_livros
 
 
 def save_bucket(df_livros):
     tempfile = NamedTemporaryFile(delete=False)
-    df_livros.to_json(tempfile, orient ='records')
+    df_livros.to_json(tempfile, orient='records')
     now = datetime.now().strftime("%Y-%m-%d")
     s3 = boto3.client("s3")
     logger.info("Salvando json no bucket..")
-    s3.put_object(Key=f'bronze/landing-date={now}/livros.json', Body=tempfile,Bucket = 'livros-scraping')
+    s3.put_object(Key=f'bronze/landing-date={now}/livros.json', Body=tempfile, Bucket='livros-scraping')
     return 'Arquivo inserido'
 
-def main(event,context):
+
+def main(event, context):
     soup = request_inicial()
     categorias_full = buscar_categorias(soup)
     lista_categorias = acertar_categorias(categorias_full)
